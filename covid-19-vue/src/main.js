30530e303e80b61c95c68133890ed4e45f836793
Arvid A <arvid.anderson@outlook.com>
2021-04-26 21:13:41 +0200

    Added firebase

--- covid-19-vue/src/main.js
+++ covid-19-vue/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import firebase from 'firebase'
+import firebaseConfig from './firebase_credentials'
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+export default db
 
 createApp(App).use(router).mount('#app')
